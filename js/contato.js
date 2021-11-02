6f56eeb52d544785b315a647f538b8c43d10a383
unknown <ellecristiny@gmail.com>
2021-11-01 20:06:27 -0400

    Versao beta

---
+++ js/contato.js
@@ -0,0 +1,8 @@
+console.log('testando arquivo js');
+
+function validarCampos(){
+
+    let form = document.forms["formContato"];
+    console.log("Nome e Sobrenome" +form.nomesobrenome);
+    return false;
+}
